993207b57297dd3307473a06bbd2e7852ff4f9be
ujwal naik <ujwal.s.naik@gmail.com>
2026-07-04 11:50:46 +0530

    Time: 38 ms (90.15%) | Memory: 54.2 MB (42.26%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -6,9 +6,6 @@
  */
 var reduce = function(nums, fn, init) {
     let j=init;
-    if (!nums.length) {
-     return init;
-   }
     for (let i= 0 ;i<nums.length;i++)
     {  
             j = fn(j,nums[i]);
