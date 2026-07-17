b0d137480157fad5bef8744af20aee703e8fd153
ujwal naik <ujwal.s.naik@gmail.com>
2026-07-17 16:51:21 +0530

    Time: 112 ms (39.72%) | Memory: 80.2 MB (23.73%) - LeetSync

---
+++ 2742-group-by/group-by.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const result ={};
+
+    for (const item of this){
+        const key = fn (item);
+        if (!result[key]){
+            result[key]=[];
+        }
+        result[key].push(item);
+    }
+    return result;
+    
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
