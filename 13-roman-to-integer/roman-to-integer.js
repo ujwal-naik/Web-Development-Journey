9b13b1334ae8cfd01684aee1c1263db085bf8e4e
ujwal naik <ujwal.s.naik@gmail.com>
2026-07-26 22:16:54 +0530

    Time: 4 ms (72.63%) | Memory: 60.8 MB (64.78%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanMap = {
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
+    };
+    let total = 0;
+    for (let i =0;i <s.length; i++){
+        let currentVal = romanMap[s[i]];
+        let nextVal = romanMap[s[i+1]];
+
+        if (currentVal < nextVal){
+            total -= currentVal;
+        }else{
+            total += currentVal;
+        }
+    }
+    return total;
+};
