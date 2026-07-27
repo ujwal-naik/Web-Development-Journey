5d3b9a0bb2cd0dfa91121db37ccf3820965796a3
ujwal naik <ujwal.s.naik@gmail.com>
2026-07-27 21:10:51 +0530

    Time: 4 ms (25.21%) | Memory: 56.3 MB (17.66%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs || strs.length === 0 ) return "";
+
+    for (let i = 0; i < strs[0].length;i++){
+        const char =  strs[0][i];
+        for (let j =1; j <strs.length ; j++){
+            if (i=== strs[j].length || strs[j][i] !== char ){
+                return strs[0].substring(0,i);
+            }
+        }
+    }
+    return strs[0];
+
+};
