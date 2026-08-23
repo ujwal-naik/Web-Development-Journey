1572372c58a2220695acf020ac3cfedc9753e621
ujwal naik <ujwal.s.naik@gmail.com>
2026-08-23 21:01:20 +0530

    Time: 5 ms (92.86%) | Memory: 59.5 MB (14.29%) - LeetSync

--- 2039-sum-game/sum-game.js
+++ 2039-sum-game/sum-game.js
@@ -7,25 +7,15 @@ var sumGame = function(num) {
     let qDiff = 0;
     let l = 0;
     let r = num.length - 1;
+    let c = 0;
 
     while (l < r) {
-        const c1 = num.charCodeAt(l);
-        if (c1 === 63) { // '?' is ASCII 63
-            qDiff++;
-        } else {
-            diff += c1 - 48; // '0' is ASCII 48
-        }
+        c = num.charCodeAt(l++);
+        c === 63 ? qDiff++ : (diff += c - 48);
 
-        const c2 = num.charCodeAt(r);
-        if (c2 === 63) {
-            qDiff--;
-        } else {
-            diff -= c2 - 48;
-        }
-
-        l++;
-        r--;
+        c = num.charCodeAt(r--);
+        c === 63 ? qDiff-- : (diff -= c - 48);
     }
 
-    return (2 * diff + 9 * qDiff) !== 0;
+    return (diff * 2 + qDiff * 9) !== 0;
 };
