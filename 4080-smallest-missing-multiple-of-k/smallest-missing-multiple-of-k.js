dd73b6c3533eef7a8bbaaeab287018da7d8d2e3b
ujwal naik <ujwal.s.naik@gmail.com>
2026-08-25 21:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (55.88%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.js
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 var missingMultiple = function(nums, k) {
-    // Determine the max bound to size the lookup table
+    
     let max = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > max) max = nums[i];
     }
 
-    // Direct indexed boolean array avoids Hash Set overhead
+    
     const seen = new Uint8Array(max + 1);
     for (let i = 0; i < nums.length; i++) {
         seen[nums[i]] = 1;
