8e8d9c6329256741e9cee2bae2083d86a6603c1f
ujwal naik <ujwal.s.naik@gmail.com>
2026-08-31 22:33:05 +0530

    Time: 5 ms (96.15%) | Memory: 85 MB (86.54%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var nodesBetweenCriticalPoints = function(head) {
+    if (!head || !head.next || !head.next.next) {
+        return [-1, -1];
+    }
+
+    let prev = head;
+    let curr = head.next;
+    let index = 1;
+
+    let firstCriticalIndex = -1;
+    let prevCriticalIndex = -1;
+    let minDistance = Infinity;
+
+    while (curr.next !== null) {
+        const isLocalMaxima = curr.val > prev.val && curr.val > curr.next.val;
+        const isLocalMinima = curr.val < prev.val && curr.val < curr.next.val;
+
+        if (isLocalMaxima || isLocalMinima) {
+            if (firstCriticalIndex === -1) {
+                firstCriticalIndex = index;
+            } else {
+                minDistance = Math.min(minDistance, index - prevCriticalIndex);
+            }
+            prevCriticalIndex = index;
+        }
+
+        prev = curr;
+        curr = curr.next;
+        index++;
+    }
+
+    if (minDistance === Infinity) {
+        return [-1, -1];
+    }
+
+    const maxDistance = prevCriticalIndex - firstCriticalIndex;
+    return [minDistance, maxDistance];
+};
